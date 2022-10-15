2fe3983309a7d99495bb626c6ab6d6e911226cb0
Лычева Екатерина <s02200433@gse.cs.msu.ru>
2022-10-15 17:47:51 +0300

    Task 1

--- 20221011/1/prog.py
+++ 20221011/1/prog.py
@@ -1,1 +1,24 @@
+from fractions import *
+
+def fun(s, w, n, a, m, b):
+    A = 0
+    for i in range(n, -1, -1):
+        A += a[n - i] * s**i
+    B = 0
+    for i in range(m, -1, -1):
+        B += b[m - i] * s**i
+    if B == 0:
+        return False
+    if A == B*w:
+        return True
+    return False
+
+st = list(map(Fraction, input().split(',')))
+s, w = st[0], st[1]
+n = int(st[2])
+a = st[3:4+n]
+m = int(st[4+n])
+b = st[5+n:]
+
+print(fun(s, w, n, a, m, b))
 
