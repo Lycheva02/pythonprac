c5d29de2c3d040b3a246ab8a70b56cf4416dbb82
Лычева Екатерина <s02200433@gse.cs.msu.ru>
2022-11-04 13:12:06 +0300

    Task 1

--- 20221101/1/prog.py
+++ 20221101/1/prog.py
@@ -1,1 +1,26 @@
-Делаем Counter - ом
+from collections import Counter
+
+class Omnibus:
+    d = Counter()
+    attrs = {}
+    def __init__(self):
+        self.attrs[id(self)] = list()
+    def __setattr__(self, key, value):
+        if key.startswith('_'):
+            return
+        if key not in self.attrs[id(self)]:
+            self.attrs[id(self)] +=  [key]
+            self.d[key] += 1
+    def __getattr__(self, name):
+        if name in self.attrs[id(self)]:
+            return self.d[name]
+    def __delattr__(self, attr):
+        if attr not in self.attrs[id(self)]:
+            return
+        self.d[attr] -= 1
+        self.attrs[id(self)].remove(attr)
+        if self.d[attr] == 0:
+            self.d.pop(attr)
+
+import sys
+exec(sys.stdin.read())
