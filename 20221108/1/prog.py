c9e7b44afa98b88c3a45c1c75f20078bc5b542d6
Лычева Екатерина <s02200433@gse.cs.msu.ru>
2022-11-14 10:51:41 +0300

    Task 1

--- 20221108/1/prog.py
+++ 20221108/1/prog.py
@@ -1,1 +1,19 @@
+from collections import UserString
 
+class DivStr(UserString):
+    def __init__(self, s=""):
+        self.data = s
+    
+    def __floordiv__(self, n):
+        k = len(self.data)//n
+        seq = [self.data[i*k:i*k+k] for i in range(n)]
+        return iter(seq)
+
+    def __mod__(self, n):
+        k = len(self.data)%n
+        if k == 0:
+            return(DivStr())
+        return DivStr(self.data[-k:])
+
+import sys
+exec(sys.stdin.read())
