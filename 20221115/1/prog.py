05413de4d0c5c88099bc36cfceffd5995617aec7
Лычева Екатерина <s02200433@gse.cs.msu.ru>
2022-11-18 23:51:21 +0300

    Task 1

--- 20221115/1/prog.py
+++ 20221115/1/prog.py
@@ -1,1 +1,19 @@
-#Ya Tut
+def objcount(cls):
+    cls.counter = 0
+    cls.___init___ = cls.__init__
+    def fun(self, *args, **kwargs):
+        cls.counter += 1
+        cls.___init___(self, *args, **kwargs)
+    cls.__init__ = fun
+    try:
+        cls.___del___ = cls.__del__
+    except:
+        cls.___del___ = lambda x: None
+    def f(self):
+        cls.counter -= 1
+        cls.___del___(self)
+    cls.__del__ = f
+    return cls
+
+import sys
+exec(sys.stdin.read())
