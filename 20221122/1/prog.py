1429d1db187d50605d3cd781efb2d6f56f5512e6
Лычева Екатерина <s02200433@gse.cs.msu.ru>
2022-11-28 01:18:57 +0300

    Task 1

--- 20221122/1/prog.py
+++ 20221122/1/prog.py
@@ -1,31 +1,13 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#
-#  prog.py
-#  
-#  Copyright 2022 Unknown <altlinux@localhost.localdomain>
-#  
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#  
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#  
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-#  MA 02110-1301, USA.
-#  
-#  
+import sys
 
+s = sys.stdin.buffer.read()
+n = s[0]
+other = s[1:]
+l = len(other)
 
-def main(args):
-    return 0
+parts = list()
+for i in range(n):
+    parts.append(other[i*l//n:(i+1)*l//n])
 
-if __name__ == '__main__':
-    import sys
-    sys.exit(main(sys.argv))
+ans = s[:1] + b''.join(sorted(parts))
+sys.stdout.buffer.write(ans)
