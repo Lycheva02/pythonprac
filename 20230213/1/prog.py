d52fec4f80326c3f8c36825dfc06f53ecbcfc66c
Lycheva Ekaterina <s02200433@gse.cs.msu.ru>
2023-02-20 01:00:25 +0300

    Пункт 3 Предыдущие коммиты случайно оказались в тестовой ветке(

--- 20230213/1/prog.py
+++ 20230213/1/prog.py
@@ -10,7 +10,7 @@ def print_last_commit(nm):
     with open('.git/objects/' + id[:2] + '/' + id[2:], 'rb') as f:
         head, _, body = zlib.decompress(f.read()).partition(b'\x00')
         print(body.decode())
-'''
+
 def print_last_commit_tree(nm):
     with open('.git/refs/heads/' + sys.argv[1], 'rb') as f:
         data = f.read()
@@ -25,7 +25,7 @@ def print_last_commit_tree(nm):
             x, y = obj.split()
             c, body = body[:20], body[20:]
             print(f"{head[:4].decode()} {c.hex()} {y.decode()}")
-
+'''
 def print_branch(nm):
     with open('.git/refs/heads/' + sys.argv[1], 'rb') as f:
         data = f.read()
@@ -59,6 +59,6 @@ if len(sys.argv) == 1:
     for b in iglob('.git/refs/heads/*'):
         print(b.split('/')[-1])
 else:
-    print_last_commit(sys.argv[1])
-#    print_last_commit_tree(sys.argv[1])
+#    print_last_commit(sys.argv[1])
+    print_last_commit_tree(sys.argv[1])
 #    print_branch(sys.argv[1])
