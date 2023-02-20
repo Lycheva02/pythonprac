1c4864e4a15c287adec6af82cb24cc2cc536d470
Лычева Екатерина <s02200433@vm-mz3-15.prac.cs.msu.ru>
2023-02-20 15:42:52 +0300

    Exercises

---
+++ 20230220/0/0/cowsay_py.py
@@ -0,0 +1,4 @@
+import sys
+from cowsay import cowsay
+
+print(cowsay(sys.argv[1]))
---
+++ 20230220/0/prog.py
@@ -0,0 +1,6 @@
+import sys
+
+sys.path.append('/usr/lib/python3/site-packages')
+sys.path.append('/usr/lib64/python3/site-packages')
+
+from pygments import *
