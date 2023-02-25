c27ef2526753b5281974e56887d243a5cd212ad4
Лычева Екатерина <s02200433@gse.cs.msu.ru>
2023-02-25 19:56:56 +0300

    Доработка команды addmon
    Доработка разбора и обработки функции addmon <x> <y> <name> <hello>, кроме проверки того, что <name> - имя "штатного" существа
    
    Добавление в обработку команды addmon проверки того, что <name> - имя "штатного" существа

--- 20230220/1/prog.py
+++ 20230220/1/prog.py
@@ -1,6 +1,7 @@
 from cowsay import cowsay
+from cowsay import list_cows
 
-gamefield = [[None for j in range(10)] for i in range(10)]
+gamefield = [[[None, None] for j in range(10)] for i in range(10)]  #[0] - name; [1] - hello
 
 def move(direction, x, y):
     match direction:
@@ -15,18 +16,22 @@ def move(direction, x, y):
     print(f"Moved to ({x}, {y})")
     return (x, y)
 
-def addmon(x, y, hello):
+def addmon(name, x, y, hello):
      if not ((0 <= x <= 9) and (0 <= y <= 9)):
         print("Invalid arguments")
         return
-     repl_flag = gamefield[x][y]
-     gamefield[x][y] = hello
-     print(f"Added monster to ({x}, {y}) saying {hello}")
+     if name not in list_cows():
+         print("Cannot add unknown monster")
+         return
+     repl_flag = gamefield[x][y][0]
+     gamefield[x][y][0] = name
+     gamefield[x][y][1] = hello
+     print(f"Added monster {name} to ({x}, {y}) saying {hello}")
      if repl_flag:
          print("Replaced the old monster")
 
 def encounter(x, y):
-    print(cowsay(gamefield[x][y]))
+    print(cowsay(gamefield[x][y][1]))
      
 
 
@@ -40,10 +45,10 @@ while True:
     match s.split():
         case ['up' | 'down' | 'left' | 'right' as direction]:
             x,y = move(direction, x, y)
-            if gamefield[x][y] != None:
+            if gamefield[x][y][0] != None:
                 encounter(x,y)
-        case 'addmon', xstr, ystr, hello:
-            addmon(int(xstr), int(ystr), hello)
+        case 'addmon', name, xstr, ystr, hello:
+            addmon(name, int(xstr), int(ystr), hello)
         case 'addmon' | 'up' | 'down' | 'left' |'right', *wtv:
             print("Invalid arguments")
         case _:
