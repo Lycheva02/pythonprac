7c19980d3cf9cf187b13d0a230a1af0e90b4b155
Лычева Екатерина <s02200433@gse.cs.msu.ru>
2023-03-05 15:07:05 +0300

    Изменён синтаксис команды addmon; монстры стали классом

--- 20230227/1/prog.py
+++ 20230227/1/prog.py
@@ -19,7 +19,12 @@ $the_cow = <<EOC;
          (((""`  `"")))
 EOC'''))
 
-gamefield = [[[None, None] for j in range(10)] for i in range(10)]  #[0] - name; [1] - hello
+class Monster:
+    def __init__(self, name, hello, hp):
+        self.name = name
+        self.hello = hello
+        self.hp = hp
+gamefield = [[None for j in range(10)] for i in range(10)]
 
 def move(direction, x, y):
     match direction:
@@ -34,27 +39,27 @@ def move(direction, x, y):
     print(f"Moved to ({x}, {y})")
     return (x, y)
 
-def addmon(name, x, y, hello):
+def addmon(name, hello, hp, x, y):
+     hp = int(hp)
      if not ((0 <= x <= 9) and (0 <= y <= 9)):
         print("Invalid arguments")
         return
      if name not in list_cows() and name != 'jgsbat':
          print("Cannot add unknown monster")
          return
-     repl_flag = gamefield[x][y][0]
-     gamefield[x][y][0] = name
-     gamefield[x][y][1] = hello
+     if hp <= 0:
+         print("Invalid hp argument")
+     repl_flag = gamefield[x][y]
+     gamefield[x][y] = Monster(name, hello, hp)
      print(f"Added monster {name} to ({x}, {y}) saying {hello}")
      if repl_flag:
          print("Replaced the old monster")
 
 def encounter(x, y):
-    if gamefield[x][y][0] == 'jgsbat':
-        print(cowsay(gamefield[x][y][1], cowfile=custom_cow))
+    if gamefield[x][y].name == 'jgsbat':
+        print(cowsay(gamefield[x][y].hello, cowfile=custom_cow))
     else:
-        print(cowsay(gamefield[x][y][1], cow=gamefield[x][y][0]))
-     
-
+        print(cowsay(gamefield[x][y].hello, cow=gamefield[x][y].name))
 
 x, y = 0, 0
 
@@ -68,10 +73,10 @@ while True:
     match s:
         case ['up' | 'down' | 'left' | 'right' as direction]:
             x,y = move(direction, x, y)
-            if gamefield[x][y][0] != None:
+            if gamefield[x][y] != None:
                 encounter(x,y)
-        case ['addmon', name, xstr, ystr, hello]:
-            addmon(name, int(xstr), int(ystr), hello)
+        case ['addmon', name, 'hello', hello, 'hp', hp, 'coords', xstr, ystr]:
+            addmon(name, hello, hp, int(xstr), int(ystr))
         case ['addmon' | 'up' | 'down' | 'left' |'right', *wtv]:
             print("Invalid arguments")
         case _:
