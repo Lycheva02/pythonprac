43af3ae690a06617d52937e851dff8e6b56220d6
Лычева Екатерина <s02200433@gse.cs.msu.ru>
2023-03-07 23:31:30 +0300

    Внедрён cmd

--- 20230306/1/prog.py
+++ 20230306/1/prog.py
@@ -30,75 +30,88 @@ class Gameplay(cmd.Cmd):
     prompt = '>>> '
     intro = "<<< Welcome to Python-MUD 0.1 >>>"
     cowlist = list_cows() + ['jgsbat']
-    
-    def __init__(self):
-        self.gamefield = [[None for j in range(10)] for i in range(10)]
-        self.x, self.y = 0, 0
+    gamefield = [[None for j in range(10)] for i in range(10)]
+    x, y = 0, 0
 
-    def encounter(x, y):
-        if gamefield[x][y].name == 'jgsbat':
-            print(cowsay(gamefield[x][y].hello, cowfile=custom_cow))
+    def encounter(self, x, y):
+        if self.gamefield[x][y].name == 'jgsbat':
+            print(cowsay(self.gamefield[x][y].hello, cowfile=custom_cow))
         else:
-            print(cowsay(gamefield[x][y].hello, cow=gamefield[x][y].name))
+            print(cowsay(self.gamefield[x][y].hello, cow=self.gamefield[x][y].name))
 
-    def do_left(self):
+    def do_left(self, args):
+        '''Move one step left'''
+        if args:
+            print("Invalid arguments")
+            return 0
         self.x = (self.x - 1)%10
         print(f"Moved to ({self.x}, {self.y})")
         if self.gamefield[self.x][self.y] != None:
                 self.encounter(self.x,self.y)
 
-    def do_right(self):
+    def do_right(self, args):
+        '''Move one step right'''
+        if args:
+            print("Invalid arguments")
+            return 0
         self.x = (self.x + 1)%10
         print(f"Moved to ({self.x}, {self.y})")
         if self.gamefield[self.x][self.y] != None:
                 self.encounter(self.x,self.y)
 
-    def do_up(self):
+    def do_up(self, args):
+        '''Move one step up'''
+        if args:
+            print("Invalid arguments")
+            return 0
         self.y = (self.y - 1)%10
         print(f"Moved to ({self.x}, {self.y})")
         if self.gamefield[self.x][self.y] != None:
                 self.encounter(self.x,self.y)
 
-    def do_down(self):
+    def do_down(self, args):
+        '''Move one step down'''
+        if args:
+            print("Invalid arguments")
+            return 0
         self.y = (self.y + 1)%10
         print(f"Moved to ({self.x}, {self.y})")
         if self.gamefield[self.x][self.y] != None:
                 self.encounter(self.x,self.y)
 
-    def do_addmon(name, hello, hp, x, y):
-         if not ((0 <= x <= 9) and (0 <= y <= 9)):
+    def do_addmon(self, args):
+        ''' Add a monster:  addmon <name> coord <x> <y> hello <message> hp <health points>'''
+        args = shlex.split(args)
+        try:
+            name = args[0]
+            hello = args[args.index('hello') + 1]
+            hp = int(args[args.index('hp') + 1])
+            coord_ind = args.index('coords')
+            x, y = list(map(int, args[coord_ind + 1: coord_ind + 3]))
+        except:
+            print("Wrong parameters")
+            return 0
+        if not ((0 <= x <= 9) and (0 <= y <= 9)):
             print("Invalid arguments")
-            return
-         if name not in self.cowlist:
-             print("Cannot add unknown monster")
-             return
-         if hp <= 0:
-             print("Invalid hp argument")
-         repl_flag = self.gamefield[x][y]
-         self.gamefield[x][y] = Monster(name, hello, hp)
-         print(f"Added monster {name} to ({x}, {y}) saying {hello}")
-         if repl_flag:
-             print("Replaced the old monster")
+            return 0
+        if name not in self.cowlist:
+            print("Cannot add unknown monster")
+            return 0
+        if hp <= 0:
+            print("Invalid hp argument")
+        repl_flag = self.gamefield[x][y]
+        self.gamefield[x][y] = Monster(name, hello, hp)
+        print(f"Added monster {name} to ({x}, {y}) saying {hello}")
+        if repl_flag:
+            print("Replaced the old monster")
 
-    def do_quit(self, args):
-        '''Выход из игры'''
+    def do_quit(self, *args):
+        '''Exit the game'''
         return 1
+
+    def default(line):
+        print("Invalid command")
 
 game = Gameplay()
 game.cmdloop()
-
-        case ['addmon', name, *parameters]:
-            try:
-                hello = parameters[parameters.index('hello') + 1]
-                hp = parameters[parameters.index('hp') + 1]
-                coord_ind = parameters.index('coords')
-                xstr, ystr = parameters[coord_ind + 1: coord_ind + 3]
-            except:
-                print("Wrong parameters")
-                continue
-            addmon(name, hello, hp, int(xstr), int(ystr))
-        case ['addmon' | 'up' | 'down' | 'left' |'right', *wtv]:
-            print("Invalid arguments")
-        case _:
-            print("Invalid command")
         
